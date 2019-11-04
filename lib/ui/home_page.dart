92f420b2a525d8e1d5fdede333d4bbcd5012cd28
Augusto Xavier <augustocbx@gmail.com>
2019-11-04 13:33:08 -0300

    Ordenação da lista de contatos

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -6,6 +6,8 @@ import 'package:agenda_contatos_flutter/ui/contact_page.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+enum OrderOptions { orderaz, orderza }
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -37,6 +39,21 @@ class _HomePageState extends State<HomePage> {
         title: Text('Contatos'),
         backgroundColor: Colors.red,
         centerTitle: true,
+        actions: <Widget>[
+          PopupMenuButton<OrderOptions>(
+            itemBuilder: (context) => <PopupMenuEntry<OrderOptions>>[
+              const PopupMenuItem<OrderOptions>(
+                child: Text("Ordenar de A-Z"),
+                value: OrderOptions.orderaz,
+              ),
+              const PopupMenuItem<OrderOptions>(
+                child: Text("Ordenar de Z-A"),
+                value: OrderOptions.orderza,
+              ),
+            ],
+            onSelected: _orderList,
+          )
+        ],
       ),
       backgroundColor: Colors.white,
       floatingActionButton: (FloatingActionButton(
@@ -71,9 +88,7 @@ class _HomePageState extends State<HomePage> {
                         image: contacts[index].img != null
                             ? FileImage(File(contacts[index].img))
                             : AssetImage("images/person.png"),
-                        fit: BoxFit.cover
-                    )
-                ),
+                        fit: BoxFit.cover)),
               ),
               Padding(
                 padding: EdgeInsets.only(left: 10.0),
@@ -109,7 +124,7 @@ class _HomePageState extends State<HomePage> {
         context: context,
         builder: (context) {
           return BottomSheet(
-            onClosing: (){},
+            onClosing: () {},
             builder: (context) {
               return Container(
                 padding: EdgeInsets.all(10.0),
@@ -119,9 +134,11 @@ class _HomePageState extends State<HomePage> {
                     Padding(
                       padding: EdgeInsets.all(10.0),
                       child: FlatButton(
-                        child: Text("Ligar",
-                          style: TextStyle(color: Colors.red, fontSize: 20.0),),
-                        onPressed: (){
+                        child: Text(
+                          "Ligar",
+                          style: TextStyle(color: Colors.red, fontSize: 20.0),
+                        ),
+                        onPressed: () {
                           launch("tel:${contacts[index].phone}");
                           Navigator.pop(context);
                         },
@@ -130,9 +147,11 @@ class _HomePageState extends State<HomePage> {
                     Padding(
                       padding: EdgeInsets.all(10.0),
                       child: FlatButton(
-                        child: Text("Editar",
-                          style: TextStyle(color: Colors.red, fontSize: 20.0),),
-                        onPressed: (){
+                        child: Text(
+                          "Editar",
+                          style: TextStyle(color: Colors.red, fontSize: 20.0),
+                        ),
+                        onPressed: () {
                           Navigator.pop(context);
                           _showContactPage(contact: contacts[index]);
                         },
@@ -141,9 +160,11 @@ class _HomePageState extends State<HomePage> {
                     Padding(
                       padding: EdgeInsets.all(10.0),
                       child: FlatButton(
-                        child: Text("Excluir",
-                          style: TextStyle(color: Colors.red, fontSize: 20.0),),
-                        onPressed: (){
+                        child: Text(
+                          "Excluir",
+                          style: TextStyle(color: Colors.red, fontSize: 20.0),
+                        ),
+                        onPressed: () {
                           helper.deleteContact(contacts[index].id);
                           setState(() {
                             contacts.removeAt(index);
@@ -178,6 +199,24 @@ class _HomePageState extends State<HomePage> {
       setState(() {
         contacts = list;
       });
+    });
+  }
+
+  void _orderList(OrderOptions result) {
+    switch (result) {
+      case OrderOptions.orderaz:
+        contacts.sort((a, b) {
+          return a.name.toLowerCase().compareTo(b.name.toLowerCase());
+        });
+        break;
+      case OrderOptions.orderza:
+        contacts.sort((a, b) {
+          return b.name.toLowerCase().compareTo(a.name.toLowerCase());
+        });
+        break;
+    }
+    setState(() {
+
     });
   }
 }
